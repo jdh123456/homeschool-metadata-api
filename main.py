6762aa93c00236c10e9b9d6a569a3c557d4c4ba7
jdh123456 <atlas_vulture8f@icloud.com>
2025-05-31 20:57:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -81,3 +81,8 @@ def get_resource_metadata():
         pass
 
     return jsonify(result)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
